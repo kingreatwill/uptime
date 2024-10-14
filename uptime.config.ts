b73e81434070ffd4c645b9cbc3934d1e19ceb560
wcoder.com <350840291@qq.com>
2024-10-14 09:17:46 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,11 +1,10 @@
 const pageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "kingreatwill's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/kingreatwill', label: 'GitHub' },
+    { link: 'https://note.wcoder.com/', label: 'Blog' },
   ],
 }
 
