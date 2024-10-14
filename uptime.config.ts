e36858e0a01d757075ef0b49570712f8f0e92a8d
wcoder.com <350840291@qq.com>
2024-10-14 09:40:46 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -27,23 +27,6 @@ const workerConfig = {
       target: 'https://note.wcoder.com',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       tooltip: "kingreatwill's profile",
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://note.wcoder.com',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] headers to be sent
-      headers: {
-        'User-Agent': 'Uptimeflare',
-      },
-      // [OPTIONAL] body to be sent
-      body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      responseKeyword: 'success',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      checkLocationWorkerRoute: 'https://note.wcoder.com',
     },   
   ],
   callbacks: {
